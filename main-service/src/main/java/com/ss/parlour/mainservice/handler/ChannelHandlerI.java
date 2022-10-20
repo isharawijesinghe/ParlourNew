07f1a4ee64f5937e7e4b2c013f5b8f72a3ccabbc
w_samurdi <w.samurdi@directfn.com>
2022-10-20 15:12:34 +0530

    resolve conflict2

--- main-service/src/main/java/com/ss/parlour/mainservice/handler/ChannelHandlerI.java
+++ main-service/src/main/java/com/ss/parlour/mainservice/handler/ChannelHandlerI.java
@@ -1,4 +1,20 @@
 package com.ss.parlour.mainservice.handler;
 
+
+import com.ss.parlour.mainservice.bean.ArticleBean;
+import com.ss.parlour.mainservice.bean.ChannelRequestBean;
+import com.ss.parlour.mainservice.bean.ChannelResponseBean;
+import com.ss.parlour.mainservice.domain.Channel;
+
+import java.util.List;
+
 public interface ChannelHandlerI {
+    ChannelResponseBean createChannel(ChannelRequestBean channel);
+    ChannelResponseBean deleteChannel(ChannelRequestBean channel);
+    ChannelResponseBean addToChannel(ArticleBean article);
+    ChannelResponseBean addToChannel(ArticleBean[] articles);
+    ChannelResponseBean viewChannels();
+    ChannelResponseBean viewChannel(int id);
+    List<ArticleBean> viewArticles(ChannelRequestBean channelRequestBean);
+
 }
