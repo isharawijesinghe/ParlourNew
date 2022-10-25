f4894db5c3adb6dd378940f1df7b6aa258a3aa83
isharawijesinghe <ishara.d.wijesinghe@gmail.com>
2022-10-26 00:23:26 +0530

    Notification service added

---
+++ notification-server/src/test/java/com/ss/parlour/notificationserver/NotificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.parlour.notificationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
