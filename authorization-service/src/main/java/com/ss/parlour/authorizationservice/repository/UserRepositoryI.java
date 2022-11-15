f194d9b43d6c4cc641d586143930ce1cdb1fbf2a
w_samurdi <w.samurdi@directfn.com>
2022-11-15 18:02:59 +0530

    channel related changes

--- authorization-service/src/main/java/com/ss/parlour/authorizationservice/repository/UserRepositoryI.java
+++ authorization-service/src/main/java/com/ss/parlour/authorizationservice/repository/UserRepositoryI.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepositoryI extends CassandraRepository<User, String> {
+public interface  UserRepositoryI extends CassandraRepository<User, String> {
 
     Optional<User> findByLoginName(String loginName);
 }
