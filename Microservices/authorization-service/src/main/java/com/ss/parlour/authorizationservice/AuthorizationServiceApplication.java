b356ae1204aa8713d5803d1885c0306abb4a2a28
isharawijesinghe <ishara.d.wijesinghe@gmail.com>
2023-01-01 23:15:33 +0530

    Disable kubernates discovery service on local deployment

--- Microservices/authorization-service/src/main/java/com/ss/parlour/authorizationservice/AuthorizationServiceApplication.java
+++ Microservices/authorization-service/src/main/java/com/ss/parlour/authorizationservice/AuthorizationServiceApplication.java
@@ -6,13 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
+@EnableEurekaClient
 @EnableDiscoveryClient
 @KubernetesApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class AuthorizationServiceApplication {
 
 	public static void main(String[] args) {
