1e0edb2bbbf6641065f90d87bc91e8c9bde882b3
isharawijesinghe <ishara.d.wijesinghe@gmail.com>
2023-01-06 11:00:17 +0530

    Updated terraform status maintain s3 and dynamodb table

--- Deployment/DevEnvironment/Terraform/Status/main.tf
+++ Deployment/DevEnvironment/Terraform/Status/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_state" {
 
-  bucket = "terraform-dev-state"
+  bucket = "parlour-terraform-dev-state"
 
   // This is only here so we can destroy the bucket as part of automated tests. You should not copy this for production
   // usage
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-dev-state-locks"
+  name         = "parlour-terraform-dev-state-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
