bdc306f8a08fcdbb11ab747839dffa311d830fe3
isharawijesinghe <ishara.d.wijesinghe@gmail.com>
2023-01-08 01:16:51 +0530

    Code refactoring added

--- Microservices/notification-server/src/main/java/com/ss/parlour/notificationserver/NotificationServerApplication.java
+++ Microservices/notification-server/src/main/java/com/ss/parlour/notificationserver/NotificationServerApplication.java
@@ -1,9 +1,15 @@
 package com.ss.parlour.notificationserver;
 
+import io.ap4k.kubernetes.annotation.KubernetesApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@KubernetesApplication
 public class NotificationServerApplication {
 
 	public static void main(String[] args) {
