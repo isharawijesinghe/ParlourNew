02e0e9921d36aa705891a6180160cbd141aae847
isharawijesinghe <ishara.d.wijesinghe@gmail.com>
2023-01-11 23:17:52 +0530

    Remove user service from microservices

--- Microservices/user-service/src/main/java/com/ss/parlour/userservice/util/validators/UserValidatorI.java
+++
@@ -1,9 +0,0 @@
-package com.ss.parlour.userservice.util.validators;
-
-import com.ss.parlour.userservice.util.bean.UserRegisterRequestBean;
-
-public interface UserValidatorI {
-
-    void validateCreateUserRequest(UserRegisterRequestBean userRegisterRequestBean);
-    void validateChangePwRequest(UserRegisterRequestBean userRegisterRequestBean);
-}
